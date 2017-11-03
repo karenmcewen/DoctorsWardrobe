eaa1948312db47eabe2d3a90f8ed973b40764826
karenmcewen <karenf_email@yahoo.com>
2017-11-03 17:59:36 -0400

    will revert

--- DoctorsWardrobe/App_Start/BundleConfig.cs
+++ DoctorsWardrobe/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace DoctorsWardrobe
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/bootstrap_Cyborg.css",
+                      //"~/Content/bootstrap_Cyborg.css",
                       "~/Content/site.css"));
         }
     }
